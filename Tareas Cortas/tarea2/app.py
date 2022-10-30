6db74e393963b28d8af95d099b7c3103238829d8
GabyGV <56004250+GabyGV@users.noreply.github.com>
2022-10-29 21:25:08 -0600

    Creation of the menu of the app

--- Tareas Cortas/tarea2/app.py
+++ Tareas Cortas/tarea2/app.py
@@ -5,4 +5,32 @@
 ############################################################
 
 #Importaciones
-import firebase
+import firebase_admin
+
+## circulos
+
+## navegacion
+
+## principal
+
+def menu():
+    print("Menú\n" 
+            "1. Circulos \n",
+            "2. Navegación \n",
+            "0. Salir\n")
+    try:
+        opcion = int(input("Opción: "))
+        if(opcion == 0):
+            return 0
+        elif(opcion == 1):
+            print("Llamar círculos")
+        elif(opcion == 2):
+            print("Llamar navegacion")
+        else:
+            error = int("error")
+        menu()
+    except:
+        print("\nOpción inválida\n")
+        menu()
+
+menu()
